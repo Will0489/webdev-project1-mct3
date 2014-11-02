1ef334913e065547645fff36a81fae74911c6d90
William Blommaert <williamblommaert@gmail.com>
2014-11-03 00:11:35 +0100

    Fixed https issue in footer

--- app/views/layouts/partials/footer.blade.php
+++ app/views/layouts/partials/footer.blade.php
@@ -2,5 +2,5 @@
 	         
 </footer>
 {{ HTML::script('https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js') }}
-{{ HTML::script('http://netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js') }}
-{{ HTML::script('assets/javascripts/application.js') }}
+{{ HTML::script('https://netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js') }}
+{{ HTML::script('assets/javascripts/application.js', null, null, true) }}
